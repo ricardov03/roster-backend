d671826451a54fe64cfee9a9847cf8955f2f5e4e
Ricardo Vargas <im@ricardovargas.me>
2023-06-17 18:35:06 -0400

    Dependency - Adding Swagger to the API.

---
+++ app/Http/Controllers/OpenApiSpecs.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\OpenApi(
+ *     @OA\Info(
+ *         version="1.0.0",
+ *         title="Roster Backend",
+ *         description="Backend API required to solve the Code Challenge defined by Zipdev + Stanbridge. This documentation includes all the existed methods for the Application.",
+ *         @OA\Contact(
+ *             email="im@ricardovargas.me"
+ *         )
+ *     ),
+ *     @OA\Server(
+ *         description="Roster Backend Server",
+ *         url="http://roster-backend.test/api/v1"
+ *     )
+ * )
+ */
+class OpenApiSpecs extends Controller
+{}
---
+++ app/Http/Controllers/OpenApiTags.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Tag(
+ *     name="user",
+ *     description="All operations related to the Users."
+ * )
+ */
+
+class OpenApiTags extends Controller
+{}
