4d3f9bd987ae510e6466d7053bb1d092bded89e1
Ricardo Vargas <im@ricardovargas.me>
2023-06-19 21:14:16 -0400

    Feature Students - Adding soft deletes.

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Enums\UserTypes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -19,6 +20,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
