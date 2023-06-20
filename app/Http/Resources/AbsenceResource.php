fdc4f2885826d386e497977bf4a12fc895fd2722
Ricardo Vargas <im@ricardovargas.me>
2023-06-20 04:09:56 -0400

    Improving Section API Endpoints. - Adding Absence.

---
+++ app/Http/Resources/AbsenceResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AbsenceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'roster' => new ShortStudentResource($this->roster->student),
+        ];
+    }
+}
--- app/Models/Absence.php
+++ app/Models/Absence.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Absence extends Model
 {
     public $timestamps = false;
+
+    public function roster()
+    {
+        return $this->belongsTo(Roster::class);
+    }
 }
